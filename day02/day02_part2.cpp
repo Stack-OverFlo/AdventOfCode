45a29d64df8e4347e402a1a70b0a65d8d4b3c32f
floriane roy <floriane.roy19@gmail.com>
2024-12-02 21:12:39 +0100

    clean code, add references, simplify ret values,

--- day02/day02_part2.cpp
+++ day02/day02_part2.cpp
@@ -3,13 +3,12 @@
 #include <string>
 #include <cmath>
 #include <vector>
-#include <algorithm>
 #include <sstream>
 
 const int MIN_DIFF = 1;
 const int MAX_DIFF = 3;
 
-bool check_if_safe(std::vector<int> level)
+bool check_if_safe(const std::vector<int> &level)
 {
   for (size_t i = 1; i < level.size(); ++i) {
     int diff_ab = std::abs(level[i] - level[i - 1]);
@@ -19,7 +18,7 @@ bool check_if_safe(std::vector<int> level)
   return true;
 }
 
-bool check_if_sorted(std::vector<int> level)
+bool check_if_sorted(const std::vector<int> &level)
 {
   bool is_sorted_ascending = true;
   bool is_sorted_descending = true;
@@ -33,64 +32,56 @@ bool check_if_sorted(std::vector<int> level)
   return is_sorted_ascending || is_sorted_descending;
 }
 
-bool check_level_validity(std::vector<int> level)
+bool check_level_validity(const std::vector<int> &level)
 {
-  bool is_sorted = false;
-  bool is_safe = false;
-
-  is_sorted = check_if_sorted(level);
-  is_safe = check_if_safe(level);
-  return is_sorted && is_safe ? true : false;
+  return check_if_sorted(level) && check_if_safe(level);
 }
 
-bool test_bad_level(const std::vector<int> level)
+bool test_bad_level(const std::vector<int> &level)
 {
-  bool is_good_level = false;
   for (size_t i = 0; i < level.size(); ++i) {
-      std::vector<int> shortened_level = level;
-      shortened_level.erase(shortened_level.begin() + i);
-      if (check_level_validity(shortened_level)) {
-	is_good_level = true;
-      }
+    std::vector<int> shortened_level(level.begin(), level.end());
+    shortened_level.erase(shortened_level.begin() + i);
+    if (check_level_validity(shortened_level)) {
+      return true;
+    }
   }
-  return is_good_level;
+  return false;
 }
 
 int get_level(const std::string &line) {
-    int value;
     std::vector<int> values_line;
+    int value;
     std::istringstream stream(line);
 
     while (stream >> value)
-      values_line.push_back(value);
-    if (values_line.size() < 2 ||
-	(!check_level_validity(values_line) && !test_bad_level(values_line)))
-      return 0;
-    std::cout << "la validité de base est : " << check_level_validity(values_line) << std::endl;
-    std::cout << "la validité avec tolerance  est : " << test_bad_level(values_line) << std::endl;
+        values_line.push_back(value);
+    if (values_line.size() < 2 || 
+        (!check_level_validity(values_line) && !test_bad_level(values_line)))
+        return 0;
     return 1;
 }
 
-void read_file(const std::string& file) {
+void read_file(const std::string &file) {
     std::string line;
     int safe_path = 0;
     std::ifstream input(file);
     if (!input.is_open()) {
-        std::cerr << "Error: Failed to open file: " << file << std::endl;
+        std::cerr << "Error: Failed to open file: " << file << "\n";
+        perror("Reason");
         return;
     }
     while (std::getline(input, line)) {
       if (!line.empty()) 
-	safe_path += get_level(line);
+        safe_path += get_level(line);
     }
-    input.close();
     std::cout << safe_path << std::endl;
 }
 
 int main(int ac, const char *av[]) {
   if (ac != 2) {
     std::cerr << "Usage: ./day02_part1 <input_file>\n";
-        return 1;
+    return 1;
   }
   read_file(av[1]);
   return 0;
